0c7bcb1f43e429e34566c26631df889d5a14dbf9
laksmanayudha <laksmanayudha22@gmail.com>
2025-02-01 11:56:49 +0800

    add onPreResonpse Hapi lifecycle

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const process = require('process');
 const notes = require('./api/notes');
 const NotesService = require('./services/inMemory/NotesService');
 const NotesValidator = require('./validator/notes');
+const ClientError = require('./exceptions/ClientError');
 
 const init = async () => {
   const notesService = new NotesService();
@@ -23,6 +24,24 @@ const init = async () => {
       service: notesService,
       validator: NotesValidator
     }
+  });
+
+  server.ext('onPreResponse', (request, h) => {
+    // mendapatkan konteks response dari request
+    const { response } = request;
+
+    // penanganan client error secara internal
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message,
+      });
+
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+
+    return h.continue;
   });
 
   await server.start();
