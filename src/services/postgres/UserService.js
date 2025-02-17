1eecdede42e70373d06c6bd05029ef04718876b5
laksmanayudha <laksmanayudha22@gmail.com>
2025-02-18 07:55:21 +0800

    add getUserById on UserService

--- src/services/postgres/UserService.js
+++ src/services/postgres/UserService.js
@@ -2,6 +2,7 @@ const { Pool } = require('pg');
 const InvariantError = require('../../exceptions/InvariantError');
 const { nanoid } = require('nanoid');
 const bcrypt = require('bcrypt');
+const NotFoundError = require('../../exceptions/NotFoundError');
 
 class UserService {
   constructor() {
@@ -41,6 +42,20 @@ class UserService {
     if (result.rows.length > 0) {
       throw new InvariantError('Gagal menambahkan user. Username sudah digunakan.');
     }
+  }
+
+  async getUserById(userId) {
+    const query = {
+      text: 'SELECT id, username, fullname FROM users WHERE id = $1',
+      values: [userId],
+    };
+
+    const result = await this._pool.query(query);
+    if (!result.rows.length) {
+      throw new NotFoundError('User tidak ditemukan');
+    }
+
+    return result.rows[0];
   }
 }
 
