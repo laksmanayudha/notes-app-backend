9f8dc94c9ac2d40641d62e7bbdff7a9621bf2b05
laksmanayudha <laksmanayudha22@gmail.com>
2025-02-25 14:08:08 +0800

    add handler, plugin, routes collaborations

--- src/server.js
+++ src/server.js
@@ -22,8 +22,14 @@ const AuthenticationsService = require('./services/postgres/AuthenticationsServi
 const TokenManager = require('./tokenize/TokenManager');
 const AuthenticationsValidator = require('./validator/authentications');
 
+// collaborations
+const collaborations = require('./api/collaborations');
+const CollaborationsService = require('./services/postgres/CollaborationsService');
+const CollaborationsValidator = require('./validator/collaborations');
+
 const init = async () => {
-  const notesService = new NotesService();
+  const collaborationsService= new CollaborationsService();
+  const notesService = new NotesService(collaborationsService);
   const usersService = new UserService();
   const authenticationsService = new AuthenticationsService();
 
@@ -86,6 +92,14 @@ const init = async () => {
         validator: AuthenticationsValidator,
       }
     },
+    {
+      plugin: collaborations,
+      options: {
+        collaborationsService,
+        notesService,
+        validator: CollaborationsValidator,
+      }
+    }
   ]);
 
   server.ext('onPreResponse', (request, h) => {
--- src/services/postgres/NotesService.js
+++ src/services/postgres/NotesService.js
@@ -6,8 +6,9 @@ const NotFoundError = require('../../exceptions/NotFoundError');
 const AuthorizationError = require('../../exceptions/AuthorizationError');
 
 class NotesService {
-  constructor() {
+  constructor(collaborationService) {
     this._pool = new Pool();
+    this._collaborationService = collaborationService;
   }
 
   async addNote({ title, body, tags, owner }) {
@@ -93,6 +94,16 @@ class NotesService {
 
     if (note.owner !== owner) {
       throw new AuthorizationError('Anda tidak berhak mengakses resource ini');
+    }
+  }
+
+  async verifyNoteAccess(noteId, userId) {
+    try {
+      await this.verifyNoteOwner(noteId, userId);
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        throw error;
+      }
     }
   }
 }
